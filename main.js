c1fc1c510040edd3ecd423dcc74f5da7910f9a08
gesha17133 <sahayuskievich@gmail.com>
2020-12-09 15:17:35 +0200

    version first

--- main.js
+++ main.js
@@ -1,15 +1,64 @@
-new Vue({
+$(document).ready(function($){
     
-    el: "#scene",
-    data: {
-        value: 1,
-        doubleValue: 1,
-    },
+    'use strict'
 
-    methods:{
-        increment( value ){
-            this.doubleValue = this.value * 2
+    let spacecraft = $('#space_ship_object'),
+    space = $('#scene'),
+    order = 0,
+    y = 85,
+    x = 45,
+    up = false,
+    right = false,
+    down = false,
+    left = false,
+    speedX = 0.3,
+    speedY = 0.3,
+    interval = 1000,
+    frequency
+    
+
+    function alertFunck(e,b){      
+        let code = e.which;
+
+        if (code == 38) {
+            up = b;
+
+        } else if (code == 40) {
+            down = b;
         }
-    },
+
+        if (code == 37) {
+            left = b;
+
+        } else if (code == 39) {
+            right = b;
+        }
+    }
+    
+    setInterval(()=>{
+
+        if(up && y > 60) y -= speedY;
+        if(down && y < 88) y += speedY;
+        if(left && x > 15) x -= speedX;
+        if(right && x < 75) x += speedX;
+
+        spacecraft.css('top',`${y}%`);
+        spacecraft.css('left',`${x}%`);
+    
+    },1)
+
+    window.setInterval(function () {
+        order += 1;
+        space.append(`<div id="metheor" class="stone_${order}"></div>`);
+        metheor = $(`.stone_${order}`);
+        let randPosX = Math.floor((Math.random() * 100) + 1);
+        metheor.animate( {top: '100%'} ,interval );
+        metheor.css('left', `${randPosX}%` ); 
+    
+    },interval);
+    
+
+    $(document).on("keyup", function(e){ alertFunck(e,false)});
+    $(document).on("keydown",function (e){ alertFunck(e,true)});
 
 })
